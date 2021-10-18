131fbdec375c65bafe724817cc8c4439432ea443
Curt Bridgers <bridgerspc@gmail.com>
2021-10-18 09:38:40 -0400

    Added entry_free and todo_remove functions that free memory associated with a listof entries and remove an entry from a list respectively

--- include/todo.h
+++ include/todo.h
@@ -3,12 +3,16 @@
 
 #define MAX_ENTRY_NAME_SIZE 128
 
-typedef struct
+typedef struct ENTRY_T
 {
     char name[MAX_ENTRY_NAME_SIZE];
     short isDone;
+    struct ENTRY_T *next;
 } ENTRY;
 
 ENTRY*  entry_create(char *name);
+void    entry_free(ENTRY *head);
+void    todo_insert(ENTRY **head, ENTRY *e);
+void    todo_remove(ENTRY **head, char *name);
 
 #endif
--- src/todo.c
+++ src/todo.c
@@ -9,5 +9,41 @@ ENTRY *entry_create(char *name)
     ENTRY *entry = (ENTRY*) malloc(sizeof(*entry));
     strncpy(entry->name, name, MAX_ENTRY_NAME_SIZE);
     entry->isDone = 0;
+    entry->next = NULL;
     return entry;
+}
+
+void todo_insert(ENTRY **head, ENTRY *e)
+{
+    e->next = *head;
+    *head = e;
+}
+
+void todo_remove(ENTRY **head, char *name)
+{
+    ENTRY *prev;
+    ENTRY *target = *head;
+
+    while (strcmp(target->name, name))
+    {
+        prev = target;
+        target = target->next;
+    }
+    fprintf(stderr, "Removing %s (found %s)\n", name, target->name);
+
+    prev->next = target->next;
+    free(target);
+    return;
+}
+
+void entry_free(ENTRY *head)
+{
+    ENTRY *tmp;
+    while (head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+    return;
 }
